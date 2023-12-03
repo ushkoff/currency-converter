e73440cb6e198aa97581d8b9905a442cc400ba08
ushkoff <lol454ushkbevise@gmail.com>
2023-12-03 21:48:21 +0200

    Fix input security

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,8 +5,8 @@ object Main extends App {
   println("Currency converter. Developer Ivan Ushkov IPZ-41, ushkov2002452@gmail.com")
 
   // Read JSON data from the file
-  // Improvement proposal #1: set up websocket solution for every-day-retrieving data from API.
-  // Improvement proposal #2: set up analogue for nodejs cron jobs for automated data updating (once in 24 hours).
+  // Improvement proposal #1: set up a websocket solution for every-day-retrieving data from API.
+  // Improvement proposal #2: set up an analogue for nodejs cron jobs for automated data updating (once in 24 hours).
   val jsonString = scala.io.Source.fromFile("currencies.json").mkString
 
   // Parse the JSON data
@@ -31,25 +31,32 @@ object Main extends App {
     // Get user input: selling or buying
     print("Do you want to (S)ell or (B)uy a currency? ")
     val operation = scala.io.StdIn.readLine().toUpperCase()
-    var finalCurrency = ""
 
     // Call the appropriate component based on the operation
     operation match {
       case "S" =>
         val (selectedCurrency, result) = SellComponent.execute(currencyData)
-        println(s"Selling Result: $result UAH for $selectedCurrency")
-        finalCurrency = selectedCurrency
+        if (selectedCurrency.nonEmpty) {
+          println(s"Selling Result: $result UAH for $selectedCurrency")
+          println("======================")
+          println(s"Найближчі до вас обмінники, де ви можете виконати обрано операцію з $selectedCurrency:")
+          println(s"https://www.google.com/maps?q=nearest+money+exchange+${selectedCurrency}+to+me&sca_esv=587494963&uact=5&gs_lp=Egxnd3Mtd2l6LXNlcnAiIG5lYXJlc3QgbW9uZXkgZXhjaGFuZ2UgVVNEIHRvIG1lMgUQIRigATIFECEYoAEyBBAhGBUyCBAhGBYYHhgdMggQIRgWGB4YHTIIECEYFhgeGB1ImAZQlgFYkQVwAHgCkAEAmAG9AaAB1AaqAQMwLja4AQPIAQD4AQHCAgQQABhHwgIHECEYoAEYCuIDBBgAIEGIBgGQBgg&um=1&ie=UTF-8&sa=X&ved=2ahUKEwjKvfKr-_OCAxXS7rsIHSVxBTYQ_AUoAXoECAEQAw")
+        } else {
+          println("You entered wrong value. Try again...")
+        }
       case "B" =>
         val (selectedCurrency, result) = BuyComponent.execute(currencyData)
-        println(s"Buying Result: $result $selectedCurrency")
-        finalCurrency = selectedCurrency
+        if (selectedCurrency.nonEmpty) {
+          println(s"Buying Result: $result $selectedCurrency")
+          println("======================")
+          println(s"Найближчі до вас обмінники, де ви можете виконати обрано операцію з $selectedCurrency:")
+          println(s"https://www.google.com/maps?q=nearest+money+exchange+${selectedCurrency}+to+me&sca_esv=587494963&uact=5&gs_lp=Egxnd3Mtd2l6LXNlcnAiIG5lYXJlc3QgbW9uZXkgZXhjaGFuZ2UgVVNEIHRvIG1lMgUQIRigATIFECEYoAEyBBAhGBUyCBAhGBYYHhgdMggQIRgWGB4YHTIIECEYFhgeGB1ImAZQlgFYkQVwAHgCkAEAmAG9AaAB1AaqAQMwLja4AQPIAQD4AQHCAgQQABhHwgIHECEYoAEYCuIDBBgAIEGIBgGQBgg&um=1&ie=UTF-8&sa=X&ved=2ahUKEwjKvfKr-_OCAxXS7rsIHSVxBTYQ_AUoAXoECAEQAw")
+        } else {
+          println("You entered wrong value. Try again...")
+        }
       case _ =>
         println("Invalid operation. Please choose (S)ell or (B)uy.")
     }
-
-    println("======================")
-    println(s"Найближчі до вас обмінники, де ви можете виконати обрано операцію з $finalCurrency:")
-    println(s"https://www.google.com/maps?q=nearest+money+exchange+${finalCurrency}+to+me&sca_esv=587494963&uact=5&gs_lp=Egxnd3Mtd2l6LXNlcnAiIG5lYXJlc3QgbW9uZXkgZXhjaGFuZ2UgVVNEIHRvIG1lMgUQIRigATIFECEYoAEyBBAhGBUyCBAhGBYYHhgdMggQIRgWGB4YHTIIECEYFhgeGB1ImAZQlgFYkQVwAHgCkAEAmAG9AaAB1AaqAQMwLja4AQPIAQD4AQHCAgQQABhHwgIHECEYoAEYCuIDBBgAIEGIBgGQBgg&um=1&ie=UTF-8&sa=X&ved=2ahUKEwjKvfKr-_OCAxXS7rsIHSVxBTYQ_AUoAXoECAEQAw")
 
     // Ask if the user wants to continue
     println("======================")
